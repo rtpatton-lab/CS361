5244370cbf1782e4d3ec3bf43c7e3e190e556982
Ryan Patton <rpatton959@gmail.com>
2021-05-16 16:40:31 -0500

    code_updated_for_better_organization

--- gui.py
+++ gui.py
@@ -1,159 +1,32 @@
-#!/usr/bin/env python
+import pandas as pd
+from pandas import DataFrame
+import requests
+import tkinter as tk
+from tkinter import *
 
-"""Convert CSV to reStructuredText tables
+# GUI portion
 
-A command-line and PythonTk GUI program to do a simple conversion from
-CSV files to reStructuredText tables
+root = tk.Tk()
 
-"""
+canvas1 = tk.Canvas(root, width=300, height=300)  # create the canvas
+canvas1.pack()
 
-import argparse
-import csv
-from tabulate import tabulate
-import tkFileDialog
-from Tkinter import *
-
-
-def get_output_filename(input_file_name):
-    """ replace the suffix of the file with .rst """
-    return input_file_name.rpartition(".")[0] + ".rst"
+entry1 = tk.Entry(root)  # create the entry box
+canvas1.create_window(150, 100, window=entry1)
 
 
-def gui():
-    """make the GUI version of this command that is run if no options are
-    provided on the command line"""
+def insert_text():  # add a function/command to be called by the button (i.e., button1 below)
 
-    def button_go_callback():
-        """ what to do when the "Go" button is pressed """
-        input_file = entry.get()
-        if input_file.rsplit(".")[-1] != "csv":
-            statusText.set("Filename must end in `.csv'")
-            message.configure(fg="red")
-            return
-        else:
-            table_contents = read_csv(input_file)
-            if table_contents is None:
-                statusText.set("Error reading file `{}'".format(input_file))
-                message.configure(fg="red")
-                return
-            output_file = get_output_filename(input_file)
-            if write_table(output_file, table_contents):
-                statusText.set("Output is in {}".format(output_file))
-                message.configure(fg="black")
-            else:
-                statusText.set("Writing file "
-                               "`{}' did not succeed".format(output_file))
-                message.configure(fg="red")
-
-    def button_browse_callback():
-        """ What to do when the Browse button is pressed """
-        filename = tkFileDialog.askopenfilename()
-        entry.delete(0, END)
-        entry.insert(0, filename)
-
-    root = Tk()
-    frame = Frame(root)
-    frame.pack()
-
-    statusText = StringVar(root)
-    statusText.set("Press Browse button or enter CSV filename, "
-                   "then press the Go button")
-
-    label = Label(root, text="CSV file: ")
-    label.pack()
-    entry = Entry(root, width=50)
-    entry.pack()
-    separator = Frame(root, height=2, bd=1, relief=SUNKEN)
-    separator.pack(fill=X, padx=5, pady=5)
-
-    button_go = Button(root,
-                       text="Go",
-                       command=button_go_callback)
-    button_browse = Button(root,
-                           text="Browse",
-                           command=button_browse_callback)
-    button_exit = Button(root,
-                         text="Exit",
-                         command=sys.exit)
-    button_go.pack()
-    button_browse.pack()
-    button_exit.pack()
-
-    separator = Frame(root, height=2, bd=1, relief=SUNKEN)
-    separator.pack(fill=X, padx=5, pady=5)
-
-    message = Label(root, textvariable=statusText)
-    message.pack()
-
-    mainloop()
+    PATH = r'sports_teams_city_state.csv'
+    read_cities = pd.read_csv(PATH)  # read the csv file using the 'PATH' variable
+    df = read_cities.loc[read_cities['Metropolitan area'].isin(['Austin'])].values[0]
+    #df = DataFrame(read_cities,
+    #               columns=['Metropolitan area', 'Country', 'Pop.rank', 'Population (2016 est.)[8]', 'B4', 'NFL', 'MLB', 'NBA', 'NHL', 'B6', 'MLS', 'CFL'])  # assign column names
+    print(df)
 
 
-def write_table(outputfile, table_contents):
-    """ Write out the .rst file with the table in it
-    """
-    with open(outputfile, "wb") as output_file:
-        try:
-            output_file.write(tabulate(table_contents,
-                                       tablefmt="grid",
-                                       headers="firstrow"))
-        except:
-            return False
-    return True
+button1 = tk.Button(root, text='Input City/State to import file', command=insert_text, bg='green',
+                    fg='white')  # button to call the 'insert_text' command above
+canvas1.create_window(150, 140, window=button1)
 
-
-def command_line(args):
-    """ Run the command-line version
-    """
-    if args.output is None:
-        args.output = get_output_filename(args.input)
-
-    table_contents = read_csv(args.input)
-
-    if write_table(args.output, table_contents):
-        print("rst table is in file `{}'".format(args.output))
-    else:
-        print("Writing file `{}' did not succeed.".format(args.output))
-
-
-def read_csv(filename):
-    """ Read the CSV file
-
-    This fails pretty silently on any exception at all
-    """
-    try:
-        with open(filename, 'rb') as csvfile:
-            dialect = csv.Sniffer().sniff(csvfile.read(1024))
-            csvfile.seek(0)
-            reader = csv.reader(csvfile, dialect)
-            r = []
-            for row in reader:
-                r.append(row)
-    except:
-        return None
-
-    return r
-
-
-def get_parser():
-    """ The argument parser of the command-line version """
-    parser = argparse.ArgumentParser(description=('convert csv to rst table'))
-
-    parser.add_argument('--input', '-F',
-                        help='name of the intput file')
-
-    parser.add_argument('--output', '-O',
-                        help=("name of the output file; " +
-                              "defaults to <inputfilename>.rst"))
-    return parser
-
-
-if __name__ == "__main__":
-    """ Run as a stand-alone script """
-
-    parser = get_parser()       # Start the command-line argument parsing
-    args = parser.parse_args()  # Read the command-line arguments
-
-    if args.input:              # If there is an argument,
-        command_line(args)      # run the command-line version
-    else:
-        gui()                   # otherwise run the GUI version
+root.mainloop()
--- working_scraper.py
+++ working_scraper.py
@@ -1,51 +1,13 @@
-"""
-
 import pandas as pd
 from pandas import DataFrame
 import requests
-from google.colab import files
+import html5lib
 import tkinter as tk
+from tkinter import *
 
 page = requests.get("https://en.wikipedia.org/wiki/List_of_American_and_Canadian_cities_by_number_of_major_professional_sports_franchises").text
 df = pd.read_html(page, flavor="bs4")[1]
 print(df)
 df.to_csv("sports_teams_city_state.csv", index=False)
 
-files.download('sports_teams_city_state.csv')
 
-"""
-
-
-# GUI portion
-
-import tkinter as tk
-from tkinter import *
-import pandas as pd
-from pandas import DataFrame
-import requests
-#from google.colab import files
-
-root = tk.Tk()
-
-canvas1 = tk.Canvas(root, width=300, height=300)  # create the canvas
-canvas1.pack()
-
-entry1 = tk.Entry(root)  # create the entry box
-canvas1.create_window(150, 100, window=entry1)
-
-
-def insert_text():  # add a function/command to be called by the button (i.e., button1 below)
-
-    PATH = r'C:/Users/young/PycharmProjects/sports_teams_city_state.csv'
-    read_cities = pd.read_csv(PATH)  # read the csv file using the 'PATH' variable
-    df = read_cities.loc[read_cities['Metropolitan area'].isin(['Austin'])].values[0]
-    #df = DataFrame(read_cities,
-    #               columns=['Metropolitan area', 'Country', 'Pop.rank', 'Population (2016 est.)[8]', 'B4', 'NFL', 'MLB', 'NBA', 'NHL', 'B6', 'MLS', 'CFL'])  # assign column names
-    print(df)
-
-
-button1 = tk.Button(root, text='Input City/State to import file', command=insert_text, bg='green',
-                    fg='white')  # button to call the 'insert_text' command above
-canvas1.create_window(150, 140, window=button1)
-
-root.mainloop()
