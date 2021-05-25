4832c84bf5a83a5308e49c504c886ed564a305ae
Ryan Patton <rpatton959@gmail.com>
2021-05-25 00:16:26 -0500

    update - sample

--- working_scraper.py
+++ working_scraper.py
@@ -6,6 +6,6 @@ import html5lib
 page = requests.get("https://en.wikipedia.org/wiki/List_of_American_and_Canadian_cities_by_number_of_major_professional_sports_franchises").text
 df = pd.read_html(page, flavor="bs4")[1]
 print(df)
-df.to_csv("sports_teams_city.csv", index=False)
+df.to_csv("sports_teams_city.csv", index=False) # sample comment for testing pushing
 
 
