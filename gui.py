66feba53bc7d6b498e64a887280ee4c316ede831
rtpatton-lab <66214517+rtpatton-lab@users.noreply.github.com>
2021-05-30 13:13:14 -0500

    updating gui

--- gui.py
+++ gui.py
@@ -8,11 +8,19 @@ from tkinter import *
 
 root = tk.Tk()
 
+root.title("Sports Teams Searched by City")
+
+root.geometry("300x300")
+
 canvas1 = tk.Canvas(root, width=300, height=300)  # create the canvas
 canvas1.pack()
 
-entry1 = tk.Entry(root)  # create the entry box
-canvas1.create_window(150, 100, window=entry1)
+top_frame_entry = tk.Entry(root)  # create the entry box
+canvas1.create_window(150, 100, window=top_frame_entry)
+top_frame_entry.pack(side="top", fill="x", padx=50, pady=10)
+
+bottom_frame_entry = Frame(root, bg="grey")
+bottom_frame_entry.pack(side="top", fill="x", padx=10, pady=10)
 
 
 def insert_text():  # add a function/command to be called by the button (i.e., button1 below)
@@ -28,5 +36,12 @@ def insert_text():  # add a function/command to be called by the button (i.e., b
 button1 = tk.Button(root, text='Input City/State to import file', command=insert_text, bg='green',
                     fg='white')  # button to call the 'insert_text' command above
 canvas1.create_window(150, 140, window=button1)
+
+scroll = Scrollbar(bottom_frame_entry)
+
+answer = Text(bottom_frame_entry, font=("Arial", 18), fg="red",
+              width=55, height=20, bd=5, yscrollcommand=scroll.set)
+answer.pack(side="left", fill="y")
+scroll.pack(side="left", fill="y")
 
 root.mainloop()
